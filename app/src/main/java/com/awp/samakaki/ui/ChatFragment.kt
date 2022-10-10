a6ca59e27f7d79e7dcb3c8854d2e223ec475e109
arifwp <arifwahyu696@gmail.com>
2022-10-11 01:18:43 +0700

    add fragment, change icon, and color

--- app/src/main/java/com/awp/samakaki/ui/ChatFragment.kt
+++ app/src/main/java/com/awp/samakaki/ui/ChatFragment.kt
@@ -1,4 +1,4 @@
-package com.awp.samakaki.ui.home
+package com.awp.samakaki.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,14 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.awp.samakaki.databinding.FragmentHomeBinding
+import com.awp.samakaki.databinding.FragmentChatBinding
 
-class HomeFragment : Fragment() {
+class ChatFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var _binding: FragmentChatBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,16 +19,9 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentChatBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
--- app/src/main/java/com/awp/samakaki/ui/MainActivity.kt
+++ app/src/main/java/com/awp/samakaki/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.awp.samakaki.ui.main
+package com.awp.samakaki.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -23,14 +23,6 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_settings
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
