54edd18f0ea49cf4691577a229fc38173fdcf6f3
Muhammad Bima <2110191059@msoffice.pens.ac.id>
2022-10-19 10:33:51 +0700

    layouting and adapter setting

---
+++ app/src/main/java/com/awp/samakaki/ChatListFragment.kt
@@ -0,0 +1,66 @@
+package com.awp.samakaki
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.awp.samakaki.databinding.FragmentChatListBinding
+import com.google.android.material.tabs.TabLayoutMediator
+
+
+class ChatListFragment : Fragment() {
+
+    private var _binding: FragmentChatListBinding? = null
+    private val binding get()= _binding!!
+    private lateinit var viewPagerAdapter: ViewPagerAdapter
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentChatListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewPagerAdapter = ViewPagerAdapter(this)
+        val toolbar = binding.toolbarHomepage
+
+        toolbar.inflateMenu(R.menu.menu_home)
+        toolbar.setOnMenuItemClickListener {
+            when(it.itemId) {
+                R.id.notification -> Toast.makeText(context, "Clicked Notification", Toast.LENGTH_SHORT).show()
+                R.id.settings -> Toast.makeText(context, "Clicked Setting", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+
+
+        with(binding){
+            vpChatList.adapter = viewPagerAdapter
+
+            TabLayoutMediator(tlChatList, vpChatList) { tab, position ->
+                when(position){
+                    0 -> tab.text = "Personal"
+                    1 -> tab.text = "Keluarga"
+                }
+            }.attach()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+
+
+}
--- app/src/main/java/com/awp/samakaki/MainActivity.kt
+++ app/src/main/java/com/awp/samakaki/MainActivity.kt
@@ -2,10 +2,25 @@ package com.awp.samakaki
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.awp.samakaki.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if(savedInstanceState == null){
+            val fragment = ChatListFragment()
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, fragment, ChatListFragment::class.simpleName)
+                .commit()
+        }
+
     }
 }
