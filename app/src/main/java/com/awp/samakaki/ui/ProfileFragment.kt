abf5125f28ee0e56838da1368ddd9c37beb0205b
anggoroedo32 <anggoroedop@gmail.com>
2022-11-09 02:29:08 +0700

    set data profile

--- app/src/main/java/com/awp/samakaki/ui/ProfileFragment.kt
+++ app/src/main/java/com/awp/samakaki/ui/ProfileFragment.kt
@@ -1,11 +1,13 @@
 package com.awp.samakaki.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.viewModels
 import androidx.fragment.app.FragmentTransaction
@@ -14,9 +16,12 @@ import com.awp.samakaki.R
 import com.awp.samakaki.adapter.PostsAdapter
 import com.awp.samakaki.databinding.ActivitySelamatDatangBinding
 import com.awp.samakaki.databinding.FragmentProfileBinding
+import com.awp.samakaki.helper.SessionManager
+import com.awp.samakaki.response.BaseResponse
 import com.awp.samakaki.viewmodel.IsiProfilViewModel
 import com.awp.samakaki.viewmodel.PostsViewModel
 import com.awp.samakaki.viewmodel.ProfileViewModel
+import com.bumptech.glide.Glide
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
@@ -30,7 +35,7 @@ class ProfileFragment : Fragment() {
     private var dateFormater: String? = null
     private var imageFile: File? = null
 
-    private val viewModel by viewModels<ProfileViewModel>()
+    private val profileViewModel by viewModels<ProfileViewModel>()
     private lateinit var postsAdapter: PostsAdapter
 
 
@@ -48,6 +53,37 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val name = binding.TVProfilename
+        val dob = binding.tvTgllahir
+        val phone = binding.tvNohp
+        val mariageStatus = binding.tvStatus
+        val avatar = binding.imgProfile
+
+
+        val token = context?.let { SessionManager.getToken(it) }
+        val id = SessionManager.getIdUser(requireContext())
+        Log.d("id_dari_edit_profile", id.toString())
+        profileViewModel.findUser(token = "Bearer $token!!", id = id.toString())
+        profileViewModel.findUser.observe(viewLifecycleOwner) {
+            when(it) {
+                is BaseResponse.Success -> {
+                    it.data
+                    name.setText(it.data?.data?.biodata?.name)
+                    dob.setText(it.data?.data?.biodata?.dob)
+                    phone.setText(it.data?.data?.biodata?.phone)
+                    mariageStatus.setText(it.data?.data?.biodata?.marriageStatus)
+
+                    Glide.with(this)
+                        .load(it.data?.data?.biodata?.avatar)
+                        .centerInside()
+                        .placeholder(R.drawable.dummy_avatar).error(R.drawable.dummy_avatar)
+                        .into(avatar)
+
+                }
+                is BaseResponse.Error -> textMessage(it.msg.toString())
+            }
+        }
+
         val btnEdProfile = binding.btnEdProfile
         btnEdProfile.setOnClickListener {
             val fragmentManager = parentFragmentManager
@@ -61,6 +97,11 @@ class ProfileFragment : Fragment() {
             fragmentTransaction.commit()
         }
     }
+
+    private fun textMessage(s: String) {
+        Toast.makeText(context,s, Toast.LENGTH_SHORT).show()
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
